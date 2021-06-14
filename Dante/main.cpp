e02dfb178ecbd4dab92cbfa76b1f659ddad98cdf
Nemanja Bartolovic <nemanjabartolovic@gmail.com>
2021-06-14 22:05:51 +0200

    Add spdlog example

--- Dante/main.cpp
+++ Dante/main.cpp
@@ -2,15 +2,31 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+
+auto logger = spdlog::stdout_color_mt("Dante");
+
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 
-int main(int arc, char** argv) {
+int main(int arc, char** argv)
+{
+    spdlog::set_pattern("[%T.%e][%n][Tid: %t][%^%l%$]: %v");
+    spdlog::set_level(spdlog::level::debug);
 
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
         std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
     }
-    else {
+    else
+    {
+        logger->info("Info example");
+        logger->warn("Here's a warning");
+        logger->error("IO error");
+
+        logger->critical("FATAL ERROR");
+
 
         auto window = SDL_CreateWindow(
             "Dante v0.0.1",
@@ -18,10 +34,13 @@ int main(int arc, char** argv) {
             SCREEN_WIDTH, SCREEN_HEIGHT,
             SDL_WINDOW_SHOWN
         );
+        logger->info("Window created");
 
         SDL_Delay(2000);
 
         SDL_DestroyWindow(window);
+
+        logger->info("Window destroyed");
     }
 
     return 0;
